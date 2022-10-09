c70ff9fa65a84ef37bb33f69580f5a61b5374be5
fabian4 <fabian4@163.com>
2022-10-09 11:39:09 +0800

    update

--- config.ts
+++ config.ts
@@ -1,10 +1,6 @@
 export const botConfig = {
-  BotName: 'ChatBot',
-  adminId: 'XXXXXXXXXXX',
-  TENCENT_SECRETID: 'XXXXXXXXXXX',
-  TENCENT_SECRETKEY: 'XXXXXXXXXXX',
-  WECHATY_PUPPET_SERVICE_TOKEN: 'XXXXXXXXXXX',
-  START_KETWORD: '谁是卧底'
+  BotName: 'bot',
+  WECHATY_PUPPET_SERVICE_TOKEN: 'XXXXXXXXXXX'
 }
 
 export default botConfig
